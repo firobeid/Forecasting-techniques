a64d4213a8c639987d4336fb3379d44f08bd277d
Firas Obeid <feras.obeid@lau.edu>
2021-03-30 11:55:10 -0500

    Old project for regression in timeseries tasks

---
+++ TimeSeriesModeling/tools.py
@@ -0,0 +1,20 @@
+from pandas import DataFrame, concat
+from csv import writer, reader
+from warnings import filterwarnings
+filterwarnings('ignore')
+
+
+
+# append new row data to existinf csv file 
+def append_list_as_row(file_name, list_of_elem):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+        
+ 
+
+
+
